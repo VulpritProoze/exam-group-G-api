1fc249bbf74a36355a3d0647c18330229b205cd7
unknown <YY504@ccs.local>
2025-04-05 14:52:35 +0800

    POST added

--- routes/route.js
+++ routes/route.js
@@ -8,4 +8,12 @@ let exams = {};
 router.get('/exam-group', (req,res) => 
     res.json({message: "Group F API"})
 );
+
+router.post('/exams', (req, res) => {
+    const newExam = req.body;
+    newExam.id = exams.length + 1; 
+    exams.push(newExam);
+    res.status(201).json(newExam);
+  });
+//   module.exports = router;
   
